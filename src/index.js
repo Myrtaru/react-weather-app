413fdbc08f9b625657a5e3019b30194c501f0502
Myrta R <80482586+Myrtaru@users.noreply.github.com>
2021-05-01 17:23:20 +0200

    updated bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { StrictMode } from "react";
-import "bootstrap/dist/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 
 
