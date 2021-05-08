beeef4612bfbbf860b97d613cbad119c4c44f187
Myrta R <80482586+Myrtaru@users.noreply.github.com>
2021-05-08 17:44:12 +0200

    added search engine

--- src/WeatherData.js
+++ src/WeatherData.js
@@ -7,50 +7,62 @@ import "./WeatherData.css";
 
 export default function Weather(props) {
     const [weatherData, setWeatherData] = useState({charged: false});
+    const [city, setCity] = useState(props.defaultCity);
      function showWeather(response) {
        setWeatherData({
-       city: response.data.name,
-       temperature: Math.round(response.data.main.temp),
-       description: response.data.weather[0].description,
-       charged: true,
-       date: new Date(response.data.dt *1000),       
-       humidity: response.data.main.humidity,
-       wind: response.data.wind.speed,
-       icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-      });
+         city: response.data.name,
+         dayTemp: Math.round(response.data.main.temp),
+         nightTemp: Math.round(response.data.main.temp_min),
+         description: response.data.weather[0].description,
+         charged: true,
+         date: new Date(response.data.dt * 1000),
+         humidity: response.data.main.humidity,
+         wind: response.data.wind.speed,
+         icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+       });
+      }
+      function searchCity() {
+        const apiKey = "d161f604274c06b1e5ec41b1728c9abc";
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(showWeather);
+      }
+       function submitSearch(event) {
+       event.preventDefault();
+       searchCity();
+      }
+      function updateCity(event) {
+        setCity(event.target.value);
       }
        if (weatherData.charged) {
   return (
     <div className="row">
       <div className="col-sm-4">
-        <WeatherOverview
-          icon={weatherData.icon}
-          description={weatherData.description}
-          DayTemp={weatherData.temperature}
-          NightTemp={weatherData.temperature}
-        />
+        <WeatherOverview overview={weatherData} />
       </div>
       <div className="col-sm-4">
         <ul>
-          <li>Last Update:</li>
+          <li>Last updated:</li>
           <li>
             <FormatDate date={weatherData.date} />
           </li>
           <h1> {weatherData.city} </h1>
           <li>Humidity: {weatherData.humidity} %</li>
           <li>Wind: {Math.round(3.6 * weatherData.wind)} km/h</li>
-          <input
-            className="search-form"
-            type="search"
-            placeholder="Enter a city"
-            autoFocus="on"
-            autoComplete="off"
-          />
-          <button type="submit" className="Button">
-            Search
-          </button>
+          <form onSubmit={submitSearch}>
+            <input
+              className="search-form"
+              type="search"
+              placeholder="Enter a city"
+              autoFocus="on"
+              onChange={updateCity}
+            />
+            <button type="submit" className="Button">
+              Search
+            </button>
+          </form>
         </ul>
       </div>
+
       <div className="col-sm-4">
         <WeatherForecast />
         <WeatherForecast />
@@ -61,9 +73,7 @@ export default function Weather(props) {
     </div>
   );  
 } else {
-   const apiKey = "d161f604274c06b1e5ec41b1728c9abc";
-   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-   axios.get(apiUrl).then(showWeather);
+   searchCity();
    return "loading...";
   }
 }
--- src/WeatherOverview.js
+++ src/WeatherOverview.js
@@ -6,10 +6,10 @@ import "./WeatherOverview.css";
 export default function WeatherOverview(props) {
   return (
     <div className="WeatherOverview">
-      <img src={props.icon} alt="Weather Icon" />
-      <p className="text-capitalize">{props.description}</p>
-      <DayTemperature DayTemp={props.DayTemp} />
-      <NightTemperature NightTemp={props.NightTemp} />
+      <img src={props.overview.icon} alt="Weather Icon" />
+      <p className="text-capitalize">{props.overview.description}</p>
+      <DayTemperature DayTemp={props.overview.dayTemp} />
+      <NightTemperature NightTemp={props.overview.nightTemp} />
     </div>
   );
 } 
